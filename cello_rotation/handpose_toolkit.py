877623a499da124566a7c8184a8db1b90b2109d6
Yitongishere <jinyt1997@163.com>
2023-12-19 16:34:34 +0800

    hand pose estimation toolkit

---
+++ cello_rotation/handpose_toolkit.py
@@ -0,0 +1,227 @@
+import numpy as np
+import os
+import json
+from icecream import ic
+from preprocess_6d import (get_mano_init, get_joint_positions,
+                            get_frame_info, weighted_average_quaternion)
+from scipy.spatial.transform import Rotation, Slerp
+from triangulation.triangulation import visualize_3d, CAM_DICT
+import matplotlib.pyplot as plt
+from triangulation.triangulation import compute_axis_lim
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+
+
+def get6d_from_txt(filepath):
+    with open(filepath) as f:
+        lines = f.readlines()
+
+    lh = np.array([float(x) for x in lines[3].rstrip().split(' ')]).reshape(16, 6)
+    rh = np.array([float(x) for x in lines[1].rstrip().split(' ')]).reshape(16, 6)
+    # ic(lh)
+    f.close()
+    return lh, rh
+
+
+def is_orthogonal(matrix):
+    transpose_matrix = np.transpose(matrix)
+    product = np.dot(matrix, transpose_matrix)
+    identity_matrix = np.identity(matrix.shape[0])
+    return np.allclose(product, identity_matrix)
+
+
+def rotation_6d_to_R(d6):
+    a1, a2 = d6[:, :3], d6[:, 3:]
+    b1 = a1 / np.linalg.norm(a1, axis=1, keepdims=True)
+    b2 = a2 - np.sum(b1 * a2, axis=1, keepdims=True) * b1
+    b2 = b2 / np.linalg.norm(b2, axis=1, keepdims=True)
+    b3 = np.cross(b1, b2)
+    b3 = b3 / np.linalg.norm(b3, axis=1, keepdims=True)
+    R = np.stack((b1, b2, b3), axis=-2)
+
+    return R
+
+
+def get_mano_init(hand_type='left'):
+
+    filepath = f"./mano_info/J3_{hand_type}.txt"
+    with open(filepath) as f:
+        lines = f.readlines()
+    init_pose = np.zeros((21, 3))
+    for l in range(len(lines)):
+        init_pose[l] = np.array([float(x) for x in lines[l].rstrip().split(' ')])
+    init_pose = init_pose - init_pose[0]
+
+    return init_pose
+
+def normalize_vector(v):
+    magnitude = np.linalg.norm(v)
+    if magnitude == 0:
+        return v
+    normalized_vector = v / magnitude
+    return normalized_vector
+
+def cal_dist(point1, point2):
+    return np.sqrt(np.sum(np.square(point1 - point2)))
+
+def get_joint_positions(positions, rotations, bone_lengths, parent_indices):
+
+    for i in range(1, 21):
+        parent_index = parent_indices[i]
+        parent_position = positions[parent_index]
+        R = rotations[parent_index]
+        while parent_index != 0:
+            parent_index = parent_indices[parent_index]
+            R = np.dot(rotations[parent_index], R)
+
+        bone_length = bone_lengths[i-1]
+        self_position = positions[i]
+
+        # 计算相对于父关节的位移
+        original_vector = bone_length * normalize_vector(self_position - parent_position)
+        relative_position = np.dot(R, original_vector)
+
+        # 累加得到全局位置
+        positions[i] = relative_position + parent_position
+
+    return positions
+
+
+def get_frame_info(proj_dir, frame_num):
+
+    cam_dirs = os.listdir(proj_dir)
+    frame_info = {}
+    for cam_dir in cam_dirs:
+        cam_num = cam_dir.split('_')[-1]
+        filepath = os.path.join(proj_dir, cam_dir, f'{frame_num+127}.txt')
+
+        lh, rh = get6d_from_txt(filepath)
+
+        R_lh = rotation_6d_to_R(lh)
+        R_rh = rotation_6d_to_R(rh)
+
+        Q_lh = Rotation.from_matrix(R_lh).as_quat()
+        Q_rh = Rotation.from_matrix(R_rh).as_quat()
+
+        frame_info[f'{cam_num}'] = {'R_lh': R_lh,
+                                    'R_rh': R_rh,
+                                    'Q_lh': Q_lh,
+                                    'Q_rh': Q_rh}
+
+    return frame_info
+
+def weighted_average_quaternion(q1, q2, w):
+    key_rots = Rotation.from_quat((q1, q2))
+
+    # 创建 Slerp 对象
+    slerp = Slerp([0, 1], key_rots)
+
+    # 进行球面线性插值
+    interpolated_quaternion = slerp(w)
+
+    return interpolated_quaternion.as_quat()
+
+def get_converted_R0(R0_cam, R0):
+    cam_R_path = '../triangulation/jsons/cello_1113_scale_camera.json'
+
+    with open(cam_R_path, 'r') as f:
+        data_dict = json.load(f)
+
+    cam_R = np.array(data_dict[R0_cam]['R']).reshape(3, 3)
+
+    converted_R0 = np.dot(cam_R, R0)
+    converted_R0 = converted_R0[np.newaxis, :]
+
+    return converted_R0
+
+
+def get_averaged_R(dir_6d, frame_num, R0_cam, cam_weights_lh, cam_weights_rh):
+    cams = list(cam_weights_lh.keys())
+    frame_info = get_frame_info(dir_6d, frame_num)
+
+    averaged_Qs_lh = []
+    averaged_Qs_rh = []
+
+    for i in range(15):
+        joint_index = i + 1
+
+        q_current_lh = frame_info[cams[0]]['Q_lh'][joint_index]
+        q_current_rh = frame_info[cams[0]]['Q_rh'][joint_index]
+
+        weights_lh = cam_weights_lh[cams[0]]
+        weights_rh = cam_weights_rh[cams[0]]
+
+        for c in range(1, len(cams)):
+            q_next_lh = frame_info[cams[c]]['Q_lh'][joint_index]
+            q_next_rh = frame_info[cams[c]]['Q_rh'][joint_index]
+
+            alpha_lh = cam_weights_lh[cams[c]] / (weights_lh + cam_weights_lh[cams[c]])
+            alpha_rh = cam_weights_rh[cams[c]] / (weights_rh + cam_weights_rh[cams[c]])
+
+            if not np.isnan(q_next_lh).any():
+                q_current_lh = weighted_average_quaternion(q_current_lh, q_next_lh, alpha_lh)
+            if not np.isnan(q_next_rh).any():
+                q_current_rh = weighted_average_quaternion(q_current_rh, q_next_rh, alpha_rh)
+
+            weights_lh += cam_weights_lh[cams[c]]
+            weights_rh += cam_weights_rh[cams[c]]
+
+        averaged_Qs_lh.append(q_current_lh)
+        averaged_Qs_rh.append(q_current_rh)
+
+    # 创建旋转对象，q 转 r
+    R_matrix_lh = Rotation.from_quat(averaged_Qs_lh).as_matrix()
+    R_matrix_rh = Rotation.from_quat(averaged_Qs_rh).as_matrix()
+
+
+    # 直接用cam0-181的所有R参数, 注释掉则用averaged R
+    #===========================================================
+    R_matrix_lh = []
+    R_matrix_rh = []
+    for i in range(15):
+        joint_index = i + 1
+        R_lh_181 = frame_info[cams[0]]['R_lh'][joint_index]
+        R_rh_181 = frame_info[cams[0]]['R_rh'][joint_index]
+        R_matrix_lh.append(R_lh_181)
+        R_matrix_rh.append(R_rh_181)
+    R_matrix_lh = np.array(R_matrix_lh)
+    R_matrix_rh = np.array(R_matrix_rh)
+    # ic(R_matrix_lh.shape)
+    #===========================================================
+
+
+    # 在最开始添加R0
+    cam_num = str(CAM_DICT[R0_cam])
+
+    R0_lh = frame_info[cam_num]['R_lh'][0]
+    converted_R0_lh = get_converted_R0(R0_cam, R0_lh)
+    R_matrix_lh = np.vstack((converted_R0_lh, R_matrix_lh))
+
+    R0_rh = frame_info[cam_num]['R_rh'][0]
+    converted_R0_rh = get_converted_R0(R0_cam, R0_rh)
+    R_matrix_rh = np.vstack((converted_R0_rh, R_matrix_rh))
+
+
+    return R_matrix_lh, R_matrix_rh
+
+
+def visualize_hand(data, connections):
+    fig = plt.figure(figsize=[10, 10])
+    axes3 = fig.add_subplot(projection="3d", computed_zorder=False)
+    xlim, ylim, zlim = compute_axis_lim(data)
+    axes3.set_xlim3d(xlim)
+    axes3.set_ylim3d(ylim)
+    axes3.set_zlim3d(zlim)
+    axes3.set_box_aspect((1, 1, 1))
+    axes3.scatter(data[:, 0], data[:, 1], data[:, 2], s=50, zorder=1)
+
+    hand_segs3d = data[tuple([connections])]
+    left_hand_coll_3d = Line3DCollection(hand_segs3d, linewidths=1, zorder=1)
+    axes3.add_collection(left_hand_coll_3d)
+    axes3.view_init(elev=30, azim=45)
+    plt.show()
+
+
+
+
+
+
---
+++ cello_rotation/integrate_handpose.py
@@ -0,0 +1,135 @@
+import json
+import numpy as np
+from icecream import ic
+from handpose_toolkit import (get_mano_init, get_joint_positions,
+                            cal_dist, get_averaged_R, visualize_hand,)
+from triangulation.triangulation import visualize_3d
+
+
+def get_bone_length_dw(kp_3d_all, frame_num):
+    target_frame = kp_3d_all[frame_num-1]
+    hands_dw = target_frame[LEFT_WRIST_INDEX:LEFT_WRIST_INDEX+42]
+    hands_dw = hands_dw.reshape(2, 21, 3)
+    hands_mano = hands_dw.copy()
+
+    for dict_id, mano_id in enumerate(MANO_TO_DW):
+        hands_mano[:, mano_id, :] = hands_dw[:, dict_id, :]
+
+    bone_lengths = np.zeros([2, 20])
+    for hand in range(2):  # two hands
+        for idx, connection in enumerate(MANO_CONNECTIONS):
+            point1 = hands_mano[hand][connection[0]]
+            point2 = hands_mano[hand][connection[1]]
+            bone_lengths[hand][idx] = cal_dist(point1, point2)
+
+    return bone_lengths
+
+
+def get_hands_joints(dir_6d, frame, bone_lengths):
+    """输出dwpose顺序的手部关键点坐标"""
+    lh_mano = get_mano_init('left')
+    rh_mano = get_mano_init('right')
+
+    lh_rot_averaged, rh_rot_averaged = get_averaged_R(dir_6d, frame, 'cam0', CAM_WEIGHTS_LH, CAM_WEIGHTS_RH)
+
+    lh_bone_length = bone_lengths[0]
+    rh_bone_length = bone_lengths[1]
+
+    lh_joints_mano = get_joint_positions(lh_mano, lh_rot_averaged, lh_bone_length, MANO_PARENTS_INDICES)
+    rh_joints_mano = get_joint_positions(rh_mano, rh_rot_averaged, rh_bone_length, MANO_PARENTS_INDICES)
+    # visualize_hand(lh_joints_mano, MANO_CONNECTIONS)
+
+    lh_joints_dw = np.zeros(lh_joints_mano.shape)
+    rh_joints_dw = np.zeros(rh_joints_mano.shape)
+    for dict_id, mano_id in enumerate(MANO_TO_DW):
+        lh_joints_dw[dict_id, :] = lh_joints_mano[mano_id, :]
+        rh_joints_dw[dict_id, :] = rh_joints_mano[mano_id, :]
+
+    return lh_joints_dw, rh_joints_dw
+
+
+CAM_WEIGHTS_LH = {
+        '21334181': 0.30,
+        '21334237': 0.10,
+        '21334190': 0.15,
+        '21334211': 0.10,
+        '21334180': 0.10,
+        '21334209': 0.05,
+        '21334221': 0.05,
+        '21334219': 0.05,
+        '21334208': 0.025,
+        '21334186': 0.025,
+        '21334184': 0.025,
+        '21334238': 0.025,
+        '21293326': 0,
+        '21293325': 0
+    }
+
+CAM_WEIGHTS_RH = {
+        '21334181': 0.30,
+        '21334237': 0.10,
+        '21334190': 0.10,
+        '21334211': 0.10,
+        '21334180': 0.10,
+        '21334209': 0.10,
+        '21334221': 0.05,
+        '21334219': 0.025,
+        '21334208': 0.025,
+        '21334186': 0.025,
+        '21334184': 0,
+        '21334238': 0.025,
+        '21293326': 0.025,
+        '21293325': 0.025
+}
+
+
+MANO_PARENTS_INDICES = [0, 0, 1, 2, 0, 4, 5, 0, 7, 8, 0, 10, 11, 0, 13, 14, 3, 6, 9, 12, 15]
+MANO_TO_DW = [0, 13, 14, 15, 20, 1, 2, 3, 16, 4, 5, 6, 17, 10, 11, 12, 19, 7, 8, 9, 18]
+
+MANO_CONNECTIONS = [(0, 1), (1, 2), (2, 3),
+                    (0, 4), (4, 5), (5, 6),
+                    (0, 7), (7, 8), (8, 9),
+                    (0, 10), (10, 11), (11, 12),
+                    (0, 13), (13, 14), (14, 15),
+                    (3, 16), (6, 17), (9, 18), (12, 19), (15, 20)]
+
+DW_CONNECTIONS = [(0, 1), (1, 2), (2, 3), (3, 4),
+                    (0, 5), (5, 6), (6, 7), (7, 8),
+                    (0, 9), (9, 10), (10, 11), (11, 12),
+                    (0, 13), (13, 14), (14, 15), (15, 16),
+                    (0, 17), (17, 18), (18, 19), (19, 20)]
+LEFT_WRIST_INDEX = 91
+RIGHT_WRIST_INDEX = 112
+
+
+if __name__ == "__main__":
+    proj_dir = "cello_1113_scale"
+    dir_6d = f"./6d_result/{proj_dir}"
+
+    with open(f'../kp_3d_result/{proj_dir}/kp_3d_all.json', 'r') as f:
+        data_dict = json.load(f)
+    kp_3d_all = np.array(data_dict['kp_3d_all'])
+
+    bone_lengths = get_bone_length_dw(kp_3d_all, 1)
+
+    for frame in range(len(kp_3d_all)):
+        lh_wrist = kp_3d_all[frame][LEFT_WRIST_INDEX]
+        rh_wrist = kp_3d_all[frame][RIGHT_WRIST_INDEX]
+
+        lh_joints, rh_joints = get_hands_joints(dir_6d, frame+1, bone_lengths)
+
+        if frame == 0:
+            visualize_hand(lh_joints, DW_CONNECTIONS)
+
+        lh_joints_pano = lh_joints + lh_wrist
+        rh_joints_pano = rh_joints + rh_wrist
+
+        kp_3d_all[frame][LEFT_WRIST_INDEX:LEFT_WRIST_INDEX+42] = np.vstack((lh_joints_pano, rh_joints_pano))
+        print(f'Hand pose integrated for frame {frame+1}.')
+
+    ic(kp_3d_all.shape)
+    visualize_3d(kp_3d_all, proj_dir, 'tri_3d_pe')
+
+    data_dict = {'kp_3d_all': kp_3d_all.tolist()}
+    with open(f'./{proj_dir}/kp_3d_all.json', 'w') as f:
+        json.dump(data_dict, f)
