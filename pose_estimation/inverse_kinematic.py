090f5e6a7f29757a9333d5af68eca8001c7fecf0
Qzping <q_ping@outlook.com>
2023-12-25 20:41:50 +0800

    Add smooth to ik

--- pose_estimation/inverse_kinematic.py
+++ pose_estimation/inverse_kinematic.py
@@ -1,6 +1,5 @@
 import contextlib
 import os.path
-
 import cv2
 import imageio
 import numpy as np
@@ -11,6 +10,8 @@ from handpose_toolkit import get6d_from_txt, rotation_6d_to_R, get_joint_positio
 from integrate_handpose import get_bone_length_dw, MANO_PARENTS_INDICES, LEFT_WRIST_INDEX, MANO_TO_DW
 import json
 from icecream import ic
+
+from triangulation.smooth import Savgol_Filter
 from triangulation.triangulation import make_projection_matrix, HUMAN_LINKS, CELLO_LINKS, BOW_LINKS, STRING_LINKS
 from scipy.optimize import minimize
 
@@ -44,64 +45,126 @@ def plot_over(img, extent=None, origin="upper", dpi=100):
     img[...] = ((255 - alpha) * img.astype(np.uint16) + alpha * rgb.astype(np.uint16)) // 255
 
 
-def visualize_overlay(proj_dir, kp_2d, frame_id):
+# def visualize_overlay(proj_dir, kp_2d, frame_id):
+#     # if not os.path.exists(f'../reproj_result/'):
+#     #     os.makedirs(f'../reproj_result/')
+#     #
+#     # if not os.path.exists(f'../reproj_result/{proj_path}/'):
+#     #     os.makedirs(f'../reproj_result/{proj_path}/')
+#     #
+#     # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#     # out = cv2.VideoWriter(f'../reproj_result/{proj_path}/output.avi', fourcc, fps=30, frameSize=[2300, 2656])
+#     fig = plt.figure(figsize=[10, 10])
+#     axes = fig.add_subplot()
+#
+#     img = imageio.v2.imread(f"../data/cello_1113/cello_1113_scale/frames/21334181/camera_21334181_{frame_id + 128}.jpg")
+#     img_with_plot = img.copy()
+#     with plot_over(img_with_plot) as axes:
+#         # axes.scatter(kp_2d[0:133, 0],
+#         #              kp_2d[0:133, 1], s=50)
+#         axes.scatter(kp_2d[0:92, 0],
+#                      kp_2d[0:92, 1], c='#1f77b4', s=50, zorder=2)
+#         axes.scatter(kp_2d[92:96, 0],
+#                      kp_2d[92:96, 1], c='grey', s=50, zorder=1)
+#         axes.scatter(kp_2d[96:133, 0],
+#                      kp_2d[96:133, 1], c='#1f77b4', s=50, zorder=2)
+#         axes.scatter(kp_2d[133:140, 0],
+#                      kp_2d[133:140, 1], c='saddlebrown', s=50)
+#         axes.scatter(kp_2d[140:142, 0],
+#                      kp_2d[140:142, 1], c='goldenrod', s=50)
+#         axes.scatter(kp_2d[142:150, 0],
+#                      kp_2d[142:150, 1], c='w', s=50)
+#         if True not in np.isnan(kp_2d[150]):
+#             axes.scatter(kp_2d[150, 0],
+#                          kp_2d[150, 1], c='r', s=25,
+#                          zorder=100)  # zorder must be the biggest so that it would not be occluded
+#             axes.scatter(kp_2d[151:155, 0],
+#                          kp_2d[151:155, 1], c='orange', s=0,
+#                          zorder=99)
+#         else:
+#             print(f'Frame {f} contact point not exist.')
+#
+#         for human in HUMAN_LINKS:
+#             plt.plot([kp_2d[human[0]][0], kp_2d[human[1]][0]], [kp_2d[human[0]][1], kp_2d[human[1]][1]], c='blue')
+#         for cello in CELLO_LINKS:
+#             plt.plot([kp_2d[cello[0]][0], kp_2d[cello[1]][0]], [kp_2d[cello[0]][1], kp_2d[cello[1]][1]],
+#                      c='saddlebrown')
+#         for bow in BOW_LINKS:
+#             plt.plot([kp_2d[bow[0]][0], kp_2d[bow[1]][0]], [kp_2d[bow[0]][1], kp_2d[bow[1]][1]], c='goldenrod')
+#
+#         for string in STRING_LINKS:
+#             plt.plot([kp_2d[string[0]][0], kp_2d[string[1]][0]], [kp_2d[string[0]][1], kp_2d[string[1]][1]], c='w')
+#
+#     img_with_plot = img_with_plot[:, :, ::-1]
+#     cv2.imwrite(f"./ik_result/{proj_dir}/ik_{frame_id + 128}.jpg", img_with_plot)
+#     # img_with_plot = cv2.resize(img_with_plot, (1150, 1328))
+#     # cv2.imshow('img', img_with_plot)
+#     # cv2.waitKey(0)
+#     plt.close()
+#     return img_with_plot
 
+
+def visualize_overlay(proj_dir, data):
+    framenum = data.shape[0]
     # if not os.path.exists(f'../reproj_result/'):
     #     os.makedirs(f'../reproj_result/')
     #
     # if not os.path.exists(f'../reproj_result/{proj_path}/'):
     #     os.makedirs(f'../reproj_result/{proj_path}/')
     #
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # out = cv2.VideoWriter(f'../reproj_result/{proj_path}/output.avi', fourcc, fps=30, frameSize=[2300, 2656])
-    fig = plt.figure(figsize=[10, 10])
-    axes = fig.add_subplot()
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(f'./ik_result/{proj_dir}/output_ik_smooth.avi', fourcc, fps=30, frameSize=[2300, 2656])
 
-    img = imageio.v2.imread(f"../data/cello_1113/cello_1113_scale/frames/21334181/camera_21334181_{frame_id+128}.jpg")
-    img_with_plot = img.copy()
-    with plot_over(img_with_plot) as axes:
-        # axes.scatter(kp_2d[0:133, 0],
-        #              kp_2d[0:133, 1], s=50)
-        axes.scatter(kp_2d[0:92, 0],
-                     kp_2d[0:92, 1], c='#1f77b4', s=50, zorder=2)
-        axes.scatter(kp_2d[92:96, 0],
-                     kp_2d[92:96, 1], c='grey', s=50, zorder=1)
-        axes.scatter(kp_2d[96:133, 0],
-                     kp_2d[96:133, 1], c='#1f77b4', s=50, zorder=2)
-        axes.scatter(kp_2d[133:140, 0],
-                     kp_2d[133:140, 1], c='saddlebrown', s=50)
-        axes.scatter(kp_2d[140:142, 0],
-                     kp_2d[140:142, 1], c='goldenrod', s=50)
-        axes.scatter(kp_2d[142:150, 0],
-                     kp_2d[142:150, 1], c='w', s=50)
-        if True not in np.isnan(kp_2d[150]):
-            axes.scatter(kp_2d[150, 0],
-                         kp_2d[150, 1], c='r', s=25,
-                         zorder=100)  # zorder must be the biggest so that it would not be occluded
-            axes.scatter(kp_2d[151:155, 0],
-                         kp_2d[151:155, 1], c='orange', s=0,
-                         zorder=99)
-        else:
-            print(f'Frame {f} contact point not exist.')
+    for f in range(framenum):
+        print(f'Frame {f} ik image generating...')
+        kp_2d = data[f]
+        fig = plt.figure(figsize=[10, 10])
+        axes = fig.add_subplot()
+        img = imageio.v2.imread(f"../data/cello_1113/cello_1113_scale/frames/21334181/camera_21334181_{f + 128}.jpg")
+        img_with_plot = img.copy()
+        with plot_over(img_with_plot) as axes:
+            # axes.scatter(kp_2d[0:133, 0],
+            #              kp_2d[0:133, 1], s=50)
+            axes.scatter(kp_2d[0:92, 0],
+                         kp_2d[0:92, 1], c='#1f77b4', s=50, zorder=2)
+            axes.scatter(kp_2d[92:96, 0],
+                         kp_2d[92:96, 1], c='grey', s=50, zorder=1)
+            axes.scatter(kp_2d[96:133, 0],
+                         kp_2d[96:133, 1], c='#1f77b4', s=50, zorder=2)
+            axes.scatter(kp_2d[133:140, 0],
+                         kp_2d[133:140, 1], c='saddlebrown', s=50)
+            axes.scatter(kp_2d[140:142, 0],
+                         kp_2d[140:142, 1], c='goldenrod', s=50)
+            axes.scatter(kp_2d[142:150, 0],
+                         kp_2d[142:150, 1], c='w', s=50)
+            if True not in np.isnan(kp_2d[150]):
+                axes.scatter(kp_2d[150, 0],
+                             kp_2d[150, 1], c='r', s=25,
+                             zorder=100)  # zorder must be the biggest so that it would not be occluded
+                axes.scatter(kp_2d[151:155, 0],
+                             kp_2d[151:155, 1], c='orange', s=0,
+                             zorder=99)
+            else:
+                print(f'Frame {f} contact point not exist.')
 
-        for human in HUMAN_LINKS:
-            plt.plot([kp_2d[human[0]][0], kp_2d[human[1]][0]], [kp_2d[human[0]][1], kp_2d[human[1]][1]], c='blue')
-        for cello in CELLO_LINKS:
-            plt.plot([kp_2d[cello[0]][0], kp_2d[cello[1]][0]], [kp_2d[cello[0]][1], kp_2d[cello[1]][1]],
-                     c='saddlebrown')
-        for bow in BOW_LINKS:
-            plt.plot([kp_2d[bow[0]][0], kp_2d[bow[1]][0]], [kp_2d[bow[0]][1], kp_2d[bow[1]][1]], c='goldenrod')
+            for human in HUMAN_LINKS:
+                plt.plot([kp_2d[human[0]][0], kp_2d[human[1]][0]], [kp_2d[human[0]][1], kp_2d[human[1]][1]], c='blue')
+            for cello in CELLO_LINKS:
+                plt.plot([kp_2d[cello[0]][0], kp_2d[cello[1]][0]], [kp_2d[cello[0]][1], kp_2d[cello[1]][1]],
+                         c='saddlebrown')
+            for bow in BOW_LINKS:
+                plt.plot([kp_2d[bow[0]][0], kp_2d[bow[1]][0]], [kp_2d[bow[0]][1], kp_2d[bow[1]][1]], c='goldenrod')
 
-        for string in STRING_LINKS:
-            plt.plot([kp_2d[string[0]][0], kp_2d[string[1]][0]], [kp_2d[string[0]][1], kp_2d[string[1]][1]], c='w')
+            for string in STRING_LINKS:
+                plt.plot([kp_2d[string[0]][0], kp_2d[string[1]][0]], [kp_2d[string[0]][1], kp_2d[string[1]][1]], c='w')
 
-    img_with_plot = img_with_plot[:, :, ::-1]
-    cv2.imwrite(f"./ik_result/{proj_dir}/ik_{frame_id+128}.jpg", img_with_plot)
-    # img_with_plot = cv2.resize(img_with_plot, (1150, 1328))
-    # cv2.imshow('img', img_with_plot)
-    # cv2.waitKey(0)
-    plt.close()
-    return img_with_plot
+        img_with_plot = img_with_plot[:, :, ::-1]
+        cv2.imwrite(f"./ik_result/{proj_dir}/ik_{frame_id + 128}.jpg", img_with_plot)
+        # img_with_plot = cv2.resize(img_with_plot, (1150, 1328))
+        # cv2.imshow('img', img_with_plot)
+        # cv2.waitKey(0)
+        out.write(img_with_plot)
+        plt.close()
 
 
 def get_rot_vec(matrix):
@@ -170,8 +233,9 @@ def obj_func(rot_vec_wrist, rot_vec_except_wrist, wrist_pos, cp_pos, finger_id, 
     pos_mano = get_joint_positions(init_pos, rot_mat, bone_length, MANO_PARENTS_INDICES)
     pos_dw = mano_to_dw(pos_mano, wrist_pos)
     tip_pos = pos_dw[DW_TIP[finger_id]]
-    dist_squared = np.sum((tip_pos - cp_pos)**2)
+    dist_squared = np.sum((tip_pos - cp_pos) ** 2)
     return dist_squared
+
 
 def find_finger(frame_num, kp_3d):
     ori_tip_position = kp_3d[frame_num][-1]
@@ -183,6 +247,7 @@ def find_finger(frame_num, kp_3d):
     used_finger_id = distances.index(min(distances))
     return used_finger_id
 
+
 def linear_interpolation(a_frame, a_displacement, b_frame, b_displacement, c_frame):
     # 计算 a 到 b 的总帧数
     total_frames = b_frame - a_frame
@@ -191,7 +256,6 @@ def linear_interpolation(a_frame, a_displacement, b_frame, b_displacement, c_fra
     # 计算 c 的位移（线性插值）
     c_displacement = a_displacement + (b_displacement - a_displacement) * (c_position / total_frames)
     return c_displacement
-
 
 
 DW_TIP = [8, 12, 16, 20]
@@ -209,21 +273,21 @@ if __name__ == '__main__':
         data_dict = json.load(f)
     kp_3d_pe = np.array(data_dict['kp_3d_all_pe'])
 
+    kp_3d_ik = kp_3d_dw.copy()
 
     if not os.path.exists('./ik_result'):
         os.mkdir('./ik_result')
     if not os.path.exists(f'./ik_result/{proj_dir}'):
         os.mkdir(f'./ik_result/{proj_dir}')
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(f'./ik_result/{proj_dir}/output_ik.avi', fourcc, fps=30, frameSize=[2300, 2656])
-
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # out = cv2.VideoWriter(f'./ik_result/{proj_dir}/output_ik.avi', fourcc, fps=30, frameSize=[2300, 2656])
 
     dir6d_path = '6d_result/cello_1113_scale/cello_1113_21334181/'
     find_first_cp = 0
     for e, frame_id in enumerate(range(kp_3d_pe.shape[0])):
-    # for e, frame_id in enumerate([37, 38, 39, 40, 41, 42, 43, 44, 45]):
+        # for e, frame_id in enumerate([37, 38, 39, 40, 41, 42, 43, 44, 45]):
         ic(frame_id)
-        file_path = os.path.join(dir6d_path, f'{128+frame_id}.txt')
+        file_path = os.path.join(dir6d_path, f'{128 + frame_id}.txt')
         left_hand, right_hand = get6d_from_txt(file_path)
         lh_rot = rotation_6d_to_R(left_hand)
         R0 = lh_rot[0]
@@ -243,7 +307,6 @@ if __name__ == '__main__':
         bls = get_bone_length_dw(kp_3d_pe, 1)
         bl = bls[0]  # IK only involves left hand
         finger = find_finger(frame_id, kp_3d_dw)
-
 
         ik_flag = 0
         frame_next = frame_id
@@ -265,7 +328,6 @@ if __name__ == '__main__':
             previous_diff_R0 = diff_R0
             previous_rotvec = Rotation.from_matrix(final_R0).as_rotvec()
 
-
         # 对于没有cp的帧操作
         cp_n = 0
         while not ik_flag:
@@ -284,7 +346,7 @@ if __name__ == '__main__':
             if not np.isnan(cp_next).any():
                 # 找到了下一个有cp的帧，此时的frame_next
                 ik_flag = 1
-                file_path_next = os.path.join(dir6d_path, f'{128+frame_next}.txt')
+                file_path_next = os.path.join(dir6d_path, f'{128 + frame_next}.txt')
                 left_hand_next, _ = get6d_from_txt(file_path_next)
                 lh_rot_next = rotation_6d_to_R(left_hand_next)
                 R0_next = lh_rot_next[0]
@@ -298,8 +360,8 @@ if __name__ == '__main__':
                 finger_next = find_finger(frame_next, kp_3d_dw)
 
                 result_next = minimize(obj_func, lh_rot_vec_wrist_next,
-                              args=(lh_rot_vec_except_wrist_next, lh_wrist_next, cp_next, finger_next, bl),
-                              method='L-BFGS-B')
+                                       args=(lh_rot_vec_except_wrist_next, lh_wrist_next, cp_next, finger_next, bl),
+                                       method='L-BFGS-B')
                 optimized_rotvec_wrist_next = result_next.x
 
                 init_R0_next = Rotation.from_rotvec(lh_rot_vec_wrist_next).as_matrix()
@@ -316,7 +378,6 @@ if __name__ == '__main__':
                 next_optimized_pos_dw = mano_to_dw(next_optimized_pos_mano, lh_wrist)
                 next_optimized_tip = next_optimized_pos_dw[DW_TIP[finger_next]]
                 next_translation = cp_next - next_optimized_tip
-
 
                 # 非视频开头，现在有了 previous_diff_R0 和 next_diff_R0，基于当前帧frame_id,转换为Q做插值
                 if find_first_cp:
@@ -369,7 +430,6 @@ if __name__ == '__main__':
         optimized_pos_dw = mano_to_dw(optimized_pos_mano, lh_wrist)
         optimized_tip = optimized_pos_dw[DW_TIP[finger]]
 
-
         # 开头第一个cp前的帧
         if frame_id in frames_before_cp:
             cp = optimized_tip
@@ -388,29 +448,57 @@ if __name__ == '__main__':
         extracted_frame[91:112] = optimized_pos_dw
         extracted_frame[112:133] = kp_3d_dw[frame_id][112:133]  # right hand should follow dw result
 
-
         extracted_frame = np.vstack((extracted_frame, kp_3d_dw[frame_id][142:]))
 
-        cam_file = "../triangulation/jsons/cello_1113_scale_camera.json"
-        cam_param = json.load(open(cam_file))
+        kp_3d_ik[frame_id] = extracted_frame
 
-        repro_2d = np.empty([155, 2])
-        repro_2d.fill(np.nan)
-        proj_mat_cam_x = make_projection_matrix(cam_param, cams=['cam0'])  # change here for various perspectives
-        for kpt in range(155):
+        # cam_file = "../triangulation/jsons/cello_1113_scale_camera.json"
+        # cam_param = json.load(open(cam_file))
+        #
+        # repro_2d = np.empty([155, 2])
+        # repro_2d.fill(np.nan)
+        # proj_mat_cam_x = make_projection_matrix(cam_param, cams=['cam0'])  # change here for various perspectives
+        # for kpt in range(155):
+        #     ones = np.ones((1))
+        #     kp4d = np.concatenate([extracted_frame[kpt], ones], axis=0)
+        #     kp4d = kp4d.reshape(-1)
+        #     # reprojection: p = mP
+        #     kp2d = np.matmul(proj_mat_cam_x, kp4d)
+        #     kp2d = kp2d.reshape((3,))
+        #     kp2d = kp2d / kp2d[2:3]
+        #     repro_2d[kpt, :] = kp2d[:2]
+        #
+        # # print(repro_2d)
+        #
+        # img = visualize_overlay(proj_dir, repro_2d, frame_id)
+        # out.write(img)
+
+    # Bow Points are currently not available, otherwise index should be set to 142
+    kp_3d_partial_ik = kp_3d_ik[:, :140, :]
+    kp_3d_partial_ik_smooth = Savgol_Filter(kp_3d_partial_ik, 140)
+    ic(kp_3d_partial_ik_smooth.shape)
+    ik_cp = kp_3d_ik[:, 140:, :]
+    kp_3d_ik_smooth = np.concatenate((kp_3d_partial_ik_smooth, ik_cp), axis=1)
+
+    framenum = kp_3d_ik_smooth.shape[0]
+    kpt_num = kp_3d_ik_smooth.shape[1]
+
+    cam_file = "../triangulation/jsons/cello_1113_scale_camera.json"
+    cam_param = json.load(open(cam_file))
+
+    # find reprojection of the specific camera
+    repro_2d = np.empty([framenum, kpt_num, 2])
+    repro_2d.fill(np.nan)
+    proj_mat_cam_x = make_projection_matrix(cam_param, cams=['cam0'])  # change here for various perspectives
+    for ff in range(framenum):
+        for kpt in range(kpt_num):
             ones = np.ones((1))
-            kp4d = np.concatenate([extracted_frame[kpt], ones], axis=0)
+            kp4d = np.concatenate([kp_3d_ik_smooth[ff][kpt], ones], axis=0)
             kp4d = kp4d.reshape(-1)
             # reprojection: p = mP
             kp2d = np.matmul(proj_mat_cam_x, kp4d)
             kp2d = kp2d.reshape((3,))
             kp2d = kp2d / kp2d[2:3]
-            repro_2d[kpt, :] = kp2d[:2]
+            repro_2d[ff, kpt, :] = kp2d[:2]
 
-        # print(repro_2d)
-
-        img = visualize_overlay(proj_dir, repro_2d, frame_id)
-        out.write(img)
-
-
-
+    visualize_overlay(proj_dir, repro_2d)
