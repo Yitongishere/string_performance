c4f52289ca86623f6b84821c4fe899f9074e18ab
shiyi099 <95453996+shiyi099@users.noreply.github.com>
2024-06-04 18:02:29 +0800

    Update script_track_key_points.py

--- script_track_key_points.py
+++ script_track_key_points.py
@@ -1,4 +1,3 @@
-import os
 """
 The script facilitates a clearer and faster execution of the project.
 This is the THIRD script.
@@ -9,15 +8,26 @@ You may need to edit: parent_dir, proj_dir, start_frame_idx, end_frame_idx, inst
 6. TRIANGULATION (Next script)
 """
 
+import os
+from tools.load_summary import get_folder, get_inform
 
-parent_dir = 'cello_1113'
-proj_dir = 'cello_1113_pgy'
-start_frame_idx = 127  # obtained by audio and video alignment
-end_frame_idx = 658
-instrument = 'cello'
-track_cams = ['21334181', '21334190', '21334237']  # cello
-# track_cams = ['21334220', '21334207']  # violin
 
+instrument = 'cello' # cello or violin
+
+if instrument == 'cello':
+    track_cams = ['21334181', '21334190']  # cello , '21334237'
+else:
+    track_cams = ['21334220', '21334207']  # violin
+
+folder_names,root_path = get_folder(instrument)
+summary = get_inform(folder_names[0],root_path)
+
+parent_dir = instrument
+
+for folder_name in folder_names:
+    proj_dir = folder_name
+    start_frame_idx = summary['StartFrame'] # obtained by audio and video alignment
+    end_frame_idx =  summary['EndFrame']# obtained by audio and video alignment
 
 """
 TRACK KEY POINTS
@@ -27,9 +37,9 @@ Multi-view 2D key points will be triangulated to obtain the 3D coordinates.
 
 os.chdir('./cello_kp_2d/')
 for track_cam in track_cams:
-    video_path = f'../data/{parent_dir}/{proj_dir}/videos/{proj_dir}_{track_cam}.avi'
-
-    trackkeypoint_command = f'python TrackKeypoints_pipeline.py ' \
+    video_path = f'../data/{parent_dir}/{proj_dir}/{proj_dir}_{track_cam}.avi'
+    #TrackKeypoints_pipeline.py
+    trackkeypoint_command = f'python3 TrackKeypoints_pipeline.py ' \
                             f'--proj_dir {proj_dir} ' \
                             f'--video_path {video_path} ' \
                             f'--start_frame_idx {start_frame_idx} ' \
