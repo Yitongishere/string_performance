288ae1dd96ece0e03672e5117d9e064e7c08ea55
shiyi099 <95453996+shiyi099@users.noreply.github.com>
2024-06-04 19:35:39 +0800

    Update script_track_key_points.py

--- script_track_key_points.py
+++ script_track_key_points.py
@@ -13,36 +13,33 @@ from tools.load_summary import get_folder, get_inform
 
 
 instrument = 'cello' # cello or violin
-
 if instrument == 'cello':
     track_cams = ['21334181', '21334190']  # cello , '21334237'
 else:
     track_cams = ['21334220', '21334207']  # violin
-
 folder_names,root_path = get_folder(instrument)
-summary = get_inform(folder_names[0],root_path)
-
 parent_dir = instrument
-
+os.chdir('./cello_kp_2d/')
 for folder_name in folder_names:
+    summary = get_inform(folder_name,root_path)
     proj_dir = folder_name
     start_frame_idx = summary['StartFrame'] # obtained by audio and video alignment
     end_frame_idx =  summary['EndFrame']# obtained by audio and video alignment
-
-"""
-TRACK KEY POINTS
-2D key points on cello/violin are collected in this section.
-Multi-view 2D key points will be triangulated to obtain the 3D coordinates.
-"""
-
-os.chdir('./cello_kp_2d/')
-for track_cam in track_cams:
-    video_path = f'../data/{parent_dir}/{proj_dir}/{proj_dir}_{track_cam}.avi'
-    #TrackKeypoints_pipeline.py
-    trackkeypoint_command = f'python3 TrackKeypoints_pipeline.py ' \
-                            f'--proj_dir {proj_dir} ' \
-                            f'--video_path {video_path} ' \
-                            f'--start_frame_idx {start_frame_idx} ' \
-                            f'--end_frame_idx {end_frame_idx} ' \
-                            f'--instrument {instrument}'
-    os.system(trackkeypoint_command)
+    
+    """
+    TRACK KEY POINTS
+    2D key points on cello/violin are collected in this section.
+    Multi-view 2D key points will be triangulated to obtain the 3D coordinates.
+    """
+    
+    for track_cam in track_cams:
+        video_path = f'../data/{parent_dir}/{proj_dir}/{proj_dir}_{track_cam}.avi'
+        #TrackKeypoints_pipeline.py
+        trackkeypoint_command = f'python3 TrackKeypoints_pipeline.py ' \
+                                f'--proj_dir {proj_dir} ' \
+                                f'--video_path {video_path} ' \
+                                f'--start_frame_idx {start_frame_idx} ' \
+                                f'--end_frame_idx {end_frame_idx} ' \
+                                f'--instrument {instrument}'
+        os.system(trackkeypoint_command)
+    #break
