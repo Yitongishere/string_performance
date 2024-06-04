3b7e85f897d7c170b88ad8cbb636e470be7e723a
shiyi099 <95453996+shiyi099@users.noreply.github.com>
2024-06-04 19:37:19 +0800

    Update script_infer.py

--- script_infer.py
+++ script_infer.py
@@ -1,4 +1,3 @@
-import os
 """
 The script facilitates a clearer and faster execution of the project.
 This is the SECOND script.
@@ -7,30 +6,45 @@ You may need to edit: parent_dir, proj_dir, end_frame_idx
 5. TRACK instrument key points (Next script)
 """
 
+import os
+from tools.load_summary import get_folder, get_inform
 
-parent_dir = 'violin_0323'
-proj_dir = 'gxianshangdeyongtandiao'
-# always start from first frame
-end_frame_idx = 4900  # could be a bit bigger than the exact end frame
-
-# parent_dir = 'cello_0111'
-# proj_dir = 'aidelizan_removed'
-# # always start from first frame
-# end_frame_idx = 1700  # could be a bit bigger than the exact end frame
-
-
-"""
-INFER
-2D key points on human are collected in this section.
-Multi-view 2D key points will be triangulated to obtain the 3D coordinates.
-"""
-
+instrument = 'cello' # cello or violin
+folder_names,root_path = get_folder(instrument)
+parent_dir = instrument
 os.chdir('./human_kp_2d/')
-dirs_path = fr'../data/{parent_dir}/{proj_dir}'
-
-infer_command = f'python infer_pipeline.py ' \
-                        f'--dirs_path {dirs_path} ' \
-                        f'--parent_dir {parent_dir} '\
-                        f'--proj_dir {proj_dir} '\
-                        f'--end_frame_idx {end_frame_idx}'
-os.system(infer_command)
+for folder_name in folder_names:
+    summary = get_inform(folder_name,root_path)
+    proj_dir = folder_name
+    # always start from first frame
+    start_frame_idx = summary['StartFrame']
+    end_frame_idx =  summary['EndFrame']# could be a bit bigger than the exact end frame
+    
+    '''
+    parent_dir = 'cello_1113'
+    proj_dir = 'cello_1113_pgy'
+    # always start from first frame
+    end_frame_idx = 660  # could be a bit bigger than the exact end frame
+    '''
+    
+    # parent_dir = 'cello_0111'
+    # proj_dir = 'aidelizan_removed'
+    # # always start from first frame
+    # end_frame_idx = 1700  # could be a bit bigger than the exact end frame
+    
+    
+    """
+    INFER
+    2D key points on human are collected in this section.
+    Multi-view 2D key points will be triangulated to obtain the 3D coordinates.
+    """
+    
+    
+    dirs_path = fr'../data/{parent_dir}/{proj_dir}'
+    
+    infer_command = f'python3 infer_pipeline.py ' \
+                            f'--dirs_path {dirs_path} ' \
+                            f'--proj_dir {proj_dir} '\
+                            f'--end_frame_idx {end_frame_idx}'
+    os.system(infer_command)
+    #break
