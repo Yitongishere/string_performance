99e6bd92eecd8554a896889e9f0c288106a72ee7
shiyi099 <95453996+shiyi099@users.noreply.github.com>
2024-06-05 16:35:50 +0800

    Update script_infer.py

--- script_infer.py
+++ script_infer.py
@@ -9,28 +9,25 @@ You may need to edit: parent_dir, proj_dir, end_frame_idx
 import os
 from tools.load_summary import get_folder, get_inform
 
+cuda = 0
+writevideo = 0
+
 instrument = 'cello' # cello or violin
 folder_names,root_path = get_folder(instrument)
+index = -2
+step = 1
+folder_names = folder_names[index-step:index]
+print(folder_names)
 parent_dir = instrument
 os.chdir('./human_kp_2d/')
+
+#cr_flag = False # The flag for covering results if existed
 for folder_name in folder_names:
     summary = get_inform(folder_name,root_path)
     proj_dir = folder_name
     # always start from first frame
     start_frame_idx = summary['StartFrame']
     end_frame_idx =  summary['EndFrame']# could be a bit bigger than the exact end frame
-    
-    '''
-    parent_dir = 'cello_1113'
-    proj_dir = 'cello_1113_pgy'
-    # always start from first frame
-    end_frame_idx = 660  # could be a bit bigger than the exact end frame
-    '''
-    
-    # parent_dir = 'cello_0111'
-    # proj_dir = 'aidelizan_removed'
-    # # always start from first frame
-    # end_frame_idx = 1700  # could be a bit bigger than the exact end frame
     
     
     """
@@ -42,10 +39,17 @@ for folder_name in folder_names:
     
     dirs_path = os.path.abspath(f'../data/{parent_dir}/{proj_dir}/')
     
+    print(f'FolderName:{summary["FolderName"]}')
+    print(f'Track:{summary["Track"]}')
+    print(f'start_frame_idx:{start_frame_idx}')
+    print(f'end_frame_idx:{end_frame_idx}')
+    
     infer_command = f'python3 infer_pipeline.py ' \
                             f'--dirs_path {dirs_path} ' \
                             f'--proj_dir {proj_dir} '\
                             f'--end_frame_idx {end_frame_idx} '\
-                            f'--start_frame_idx {start_frame_idx}'
+                            f'--start_frame_idx {start_frame_idx} '\
+                            f'--cuda {cuda} '\
+                            f'--writevideo {writevideo}'
     os.system(infer_command)
     #break
