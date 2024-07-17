5f8257e797b4d9e0b53c89f54d7d264d459c3b8b
shiyi099 <95453996+shiyi099@users.noreply.github.com>
2024-07-17 20:48:37 +0800

    Update integrate_handpose_pipeline.py

--- pose_estimation/integrate_handpose_pipeline.py
+++ pose_estimation/integrate_handpose_pipeline.py
@@ -43,7 +43,7 @@ def get_hand_length(hands_dw):
     return bone_lengths
 
 
-def get_hands_joints(dir_6d, frame, bone_lengths, cam_file_path, show_cam, instrument, parent_dir, proj_dir):
+def get_hands_joints(dir_6d, frame, bone_lengths, cam_param, show_cam, instrument, parent_dir, proj_dir, cam_drop_frames = None):
     """输出dwpose顺序的手部关键点坐标"""
     lh_mano = get_mano_init('left')
     rh_mano = get_mano_init('right')
@@ -54,50 +54,72 @@ def get_hands_joints(dir_6d, frame, bone_lengths, cam_file_path, show_cam, instr
     else:
         cam_weight_lh = CAM_WEIGHTS_LH_VIOLIN.copy()
         cam_weight_rh = CAM_WEIGHTS_RH_VIOLIN.copy()
+
+    dirs = os.listdir(dir_6d)
+    run_cams = [i.split('_')[-1] for i in dirs]
+
+    cam_weight_lh = {run_cam: cam_weight_lh[run_cam] for run_cam in run_cams if run_cam in cam_weight_lh}
+    cam_weight_rh = {run_cam: cam_weight_rh[run_cam] for run_cam in run_cams if run_cam in cam_weight_rh}
+
     keys = list(cam_weight_lh.keys())
-    values = list(cam_weight_rh.values())
-    frame_num_dict = {}
-    for key in keys:
-        actual_frame = frame
-        frame_num_dict[f'{key}'] = actual_frame
-        try:
-            frame_drop_path = f'../data/{parent_dir}/{proj_dir}/videos/{key}_FrameDropIDLog.txt'
-            drop_frames = np.array(open(frame_drop_path).readlines(), dtype=int)
-            dropped = 0
-            for drop_frame in drop_frames:
-                if drop_frame < frame:
-                    dropped += 1
-                elif drop_frame == frame:
-                    print(f'Remove cam {key} for frame {frame}!')
-                    values.remove(cam_weight_lh[key])
-                    cam_weight_lh[key] = 0
-                    for k, v in cam_weight_lh.items():
-                        cam_weight_lh[k] = v / sum(values)
-                    values = list(cam_weight_lh.values())
-                    break
-                elif drop_frame > frame:
-                    break
-            actual_frame -= dropped
+    values = list(cam_weight_lh.values())
+
+    for cam_weight in [cam_weight_lh, cam_weight_rh]:
+        keys = list(cam_weight.keys())
+        values = list(cam_weight.values())
+        frame_num_dict = {}
+        for key in keys:
+            actual_frame = frame
             frame_num_dict[f'{key}'] = actual_frame
-            # if frame in drop_frames:
-            #     print(f'Remove cam {key} for frame {frame}!')
-            #     values.remove(cam_weight_lh[key])
-            #     cam_weight_lh[key] = 0
-            #     for k, v in cam_weight_lh.items():
-            #         cam_weight_lh[k] = v / sum(values)
-            #     values = list(cam_weight_lh.values())
-        except FileNotFoundError as e:
-            pass
+            try:
+                
+                #frame_drop_path = f'../data/{parent_dir}/{proj_dir}/videos/{key}_FrameDropIDLog.txt'
+                #drop_frames = np.array(open(frame_drop_path).readlines(), dtype=int)
+                
+                if cam_drop_frames is not None:
+                    drop_frames = [] if cam_drop_frames[key] == None else np.array(cam_drop_frames[key],dtype = int)
+                
+                dropped = 0
+                for drop_frame in drop_frames:
+                    if drop_frame < frame:
+                        dropped += 1
+                    elif drop_frame == frame:
+                        print(f'Remove cam {key} for frame {frame}!')
+                        values.remove(cam_weight[key])
+                        cam_weight[key] = 0
+                        for k, v in cam_weight.items():
+                            cam_weight[k] = v / sum(values)
+                        values = list(cam_weight.values())
+                        break
+                    elif drop_frame > frame:
+                        break
+                actual_frame -= dropped
+                frame_num_dict[f'{key}'] = actual_frame
+                # if frame in drop_frames:
+                #     print(f'Remove cam {key} for frame {frame}!')
+                #     values.remove(cam_weight_lh[key])
+                #     cam_weight_lh[key] = 0
+                #     for k, v in cam_weight_lh.items():
+                #         cam_weight_lh[k] = v / sum(values)
+                #     values = list(cam_weight_lh.values())
+            except FileNotFoundError as e:
+                pass
 
     cam_weight_lh = dict(sorted(cam_weight_lh.items(), key=lambda x: x[1], reverse=True))
+    cam_weight_rh = dict(sorted(cam_weight_rh.items(), key=lambda x: x[1], reverse=True))
+
+    # ic(frame_num_dict)
 
     frame_info = get_frame_info(dir_6d, frame_num_dict)
 
-    ic(cam_weight_lh)
-    ic(frame_num_dict)
+    # ic(cam_weight_lh)
+    # ic(frame_num_dict)
 
-    lh_rot_averaged, rh_rot_averaged = get_averaged_R(frame_info, show_cam, cam_weight_lh, cam_weight_rh,
-                                                      cam_file_path)
+    lh_rot_averaged, rh_rot_averaged = get_averaged_R(frame_info,
+                                                      show_cam,
+                                                      cam_weight_lh,
+                                                      cam_weight_rh,
+                                                      cam_param)
 
     lh_bone_length = bone_lengths[0]
     rh_bone_length = bone_lengths[1]
@@ -116,7 +138,8 @@ def get_hands_joints(dir_6d, frame, bone_lengths, cam_file_path, show_cam, instr
 
 
 def get_lh_bone_length(proj_dir):
-    with open(f'../audio/cp_result/{proj_dir}/kp_3d_all_dw_cp_smooth.json', 'r') as f:
+    print(os.path.abspath(f'../audio/cp_result/{parent_dir}/{proj_dir}/kp_3d_all_dw_cp_smooth.json'))
+    with open(f'../audio/cp_result/{parent_dir}/{proj_dir}/kp_3d_all_dw_cp_smooth.json', 'r') as f:
         data_dict = json.load(f)
     kp_3d_dw = np.array(data_dict['kp_3d_all_dw_cp_smooth'])
 
@@ -161,66 +184,100 @@ def get_lh_bone_length(proj_dir):
 
 CAM_WEIGHTS_LH_VIOLIN = {
     # 7
-    '21334207': 0.4,
-    '21334191': 0.1,
-    '21334182': 0.05,
+    '21334207': 0.3,
+    '21334191': 0.,
+    '21334182': 0.,
     # 6
-    '21334220': 0.4,  # TODO reduce weight
-    # '21334236': 0,  # TODO add weight
-    '21334183': 0.05,
+    '21334220': 0.3,  
+    '21334236': 0.3,  
+    '21334183': 0.,
     # 5
-    '21293324': 0,
+    '21293324': 0.1,
     '21334218': 0,
     '21334206': 0
 }
 
 CAM_WEIGHTS_RH_VIOLIN = {
     # 7
-    '21334207': 0.2,
-    '21334191': 0.1,
-    '21334182': 0.05,
+    '21334207': 0.3,
+    '21334191': 0.,
+    '21334182': 0.,
     # 6
-    '21334220': 0.6,  # TODO reduce weight
-    # '21334236': 0,  # TODO add weight
-    '21334183': 0.05,
+    '21334220': 0.3,
+    '21334236': 0.3,
+    '21334183': 0.,
     # 5
-    '21293324': 0,
+    '21293324': 0.1,
     '21334218': 0,
     '21334206': 0
 }
 
+# CAM_WEIGHTS_LH_CELLO = {
+#     '21334181': 0.30,
+#     '21334237': 0.10,
+#     '21334190': 0.15,
+#     '21334211': 0.10,
+#     '21334180': 0.10,
+#     '21334209': 0.05,
+#     '21334221': 0.05,
+#     '21334219': 0.05,
+#     '21334208': 0.025,
+#     '21334186': 0.025,
+#     '21334184': 0.025,  # TODO remove
+#     '21334238': 0.025,  # TODO remove
+#     '21293326': 0,
+#     '21293325': 0
+# }
+
 CAM_WEIGHTS_LH_CELLO = {
-    '21334181': 0.30,
-    '21334237': 0.10,
-    '21334190': 0.15,
-    '21334211': 0.10,
-    '21334180': 0.10,
-    '21334209': 0.05,
-    '21334221': 0.05,
-    '21334219': 0.05,
-    '21334208': 0.025,
-    '21334186': 0.025,
-    '21334184': 0.025,  # TODO remove
-    '21334238': 0.025,  # TODO remove
-    '21293326': 0,
-    '21293325': 0
+    '21334181': 0.65,
+    '21334237': 0.,
+    '21334190': 0.3,
+    '21334211': 0.,
+    '21334180': 0.05,
+    '21334209': 0.,
+    '21334221': 0.,
+    '21334219': 0.,
+    '21334208': 0.,
+    '21334186': 0.,
+    '21334184': 0.,  # TODO remove
+    '21334238': 0.,  # TODO remove
+    '21293326': 0.,
+    '21293325': 0.
 }
 
+# CAM_WEIGHTS_RH_CELLO = {
+#     '21334181': 0.30,
+#     '21334237': 0.10,
+#     '21334190': 0.10,
+#     '21334211': 0.10,
+#     '21334180': 0.10,
+#     '21334209': 0.10,
+#     '21334221': 0.05,
+#     '21334219': 0.025,
+#     '21334208': 0.025,
+#     '21334186': 0.025,
+#     '21334184': 0,
+#     '21334238': 0.025,
+#     '21293326': 0.025,
+#     '21293325': 0.025
+# }
+
 CAM_WEIGHTS_RH_CELLO = {
-    '21334181': 0.30,
-    '21334237': 0.10,
-    '21334190': 0.10,
-    '21334211': 0.10,
-    '21334180': 0.10,
-    '21334209': 0.10,
-    '21334221': 0.05,
-    '21334219': 0.025,
-    '21334208': 0.025,
-    '21334186': 0.025,
-    '21334184': 0,
-    '21334238': 0.025,
-    '21293326': 0.025,
-    '21293325': 0.025
+    '21334181': 0.65,
+    '21334237': 0.,
+    '21334190': 0.3,
+    '21334211': 0.,
+    '21334180': 0.05,
+    '21334209': 0.,
+    '21334221': 0.,
+    '21334219': 0.,
+    '21334208': 0.,
+    '21334186': 0.,
+    '21334184': 0.,  # TODO remove
+    '21334238': 0.,  # TODO remove
+    '21293326': 0.,
+    '21293325': 0.
 }
 
 MANO_PARENTS_INDICES = [0, 0, 1, 2, 0, 4, 5, 0, 7, 8, 0, 10, 11, 0, 13, 14, 3, 6, 9, 12, 15]
@@ -241,28 +298,43 @@ DW_CONNECTIONS = [(0, 1), (1, 2), (2, 3), (3, 4),
 LEFT_WRIST_INDEX = 91
 RIGHT_WRIST_INDEX = 112
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='integrate_handpose_pipeline')
-    parser.add_argument('--cam_file', default='../triangulation/jsons/cello_1113_scale_camera.json', type=str,
+    parser.add_argument('--summary_jsonfile',
+                        default='../data/cello/cello01/cello01_summary.json',
+                        type=str, 
                         required=True)
-    parser.add_argument('--parent_dir', default='cello_1113', type=str, required=True)
-    parser.add_argument('--proj_dir', default='cello_1113_scale', type=str, required=True)
-    parser.add_argument('--start_frame', default=128, type=str, required=True)
+    parser.add_argument('--parent_dir', default='cello', type=str, required=True)
+    parser.add_argument('--proj_dir', default='cello01', type=str, required=True)
+    parser.add_argument('--start_frame', default='128', type=int, required=True)
+    parser.add_argument('--end_frame', default='786', type=int, required=False)
     parser.add_argument('--instrument', default='cello', type=str, required=True)
     parser.add_argument('--cam_num', default='cam0', type=str, required=True)
+    
     args = parser.parse_args()
-    cam_file = args.cam_file
+    with open(args.summary_jsonfile,'r') as f:
+        summary = json.load(f)
+    f.close()
+    
     parent_dir = args.parent_dir
     proj_dir = args.proj_dir
-    start_frame = int(args.start_frame)
+    start_frame = args.start_frame
+    end_frame = args.end_frame
     instrument = args.instrument
     cam_num = args.cam_num
-
+    
+    cam_param = summary['CameraParameter']
+    cam_drop_frames = summary['FrameDropIDLog']
+    
+    start_frame = summary['StartFrame']
+    end_frame = summary['EndFrame']
+    
     # proj_dir = "cello_1113_scale"
     # start_frame = 128
     # cam_file = '../triangulation/jsons/cello_1113_scale_camera.json'
 
-    dir_6d = f"./6d_result/{proj_dir}"
+    dir_6d = f"./6d_result/{parent_dir}/{proj_dir}"
 
     lh_bone_length = get_lh_bone_length(proj_dir)
 
@@ -270,7 +342,7 @@ if __name__ == "__main__":
     #     data_dict = json.load(f)
     # kp_3d_all = np.array(data_dict['kp_3d_all_dw'])
 
-    with open(f'../audio/cp_result/{proj_dir}/kp_3d_all_dw_cp.json', 'r') as f:
+    with open(f'../audio/cp_result/{parent_dir}/{proj_dir}/kp_3d_all_dw_cp.json', 'r') as f:
         data_dict = json.load(f)
     kp_3d_all = np.array(data_dict['kp_3d_all_dw_cp'])
 
@@ -283,9 +355,11 @@ if __name__ == "__main__":
     for frame in range(len(kp_3d_all)):
         lh_wrist = kp_3d_all[frame][LEFT_WRIST_INDEX]
         rh_wrist = kp_3d_all[frame][RIGHT_WRIST_INDEX]
-
-        lh_joints, rh_joints, lh_rot, rh_rot = get_hands_joints(dir_6d, frame + start_frame, bone_lengths, cam_file,
-                                                                cam_num, instrument, parent_dir, proj_dir)
+        lh_joints, rh_joints, lh_rot, rh_rot = get_hands_joints(dir_6d, frame + start_frame, 
+                                                                bone_lengths, cam_param,
+                                                                cam_num, instrument, 
+                                                                parent_dir, proj_dir,
+                                                                cam_drop_frames)
 
         hand_rot = np.concatenate([lh_rot, rh_rot], axis=0)
         integrated_hand_rot.append(hand_rot.tolist())
@@ -298,7 +372,10 @@ if __name__ == "__main__":
 
         # kp_3d_all[frame][LEFT_WRIST_INDEX:LEFT_WRIST_INDEX+42] = np.vstack((lh_joints_pano, rh_joints_pano))
         kp_3d_all[frame][LEFT_WRIST_INDEX:RIGHT_WRIST_INDEX] = lh_joints_pano
-        print(f'Hand pose integrated for frame {frame + start_frame}.')
+        kp_3d_all[frame][RIGHT_WRIST_INDEX:RIGHT_WRIST_INDEX+21] = rh_joints_pano
+
+        
+        print(f'Hand pose integrated for frame {frame + start_frame}. -> [{proj_dir}]')
 
     ic(kp_3d_all.shape)
     # visualize_3d(kp_3d_all, proj_dir, 'integrated_fk_3d_pe')
@@ -310,13 +387,13 @@ if __name__ == "__main__":
     # with open(f'../kp_3d_result/{proj_dir}/kp_3d_all_pe.json', 'w') as f:
     #     json.dump(data_dict, f)
 
-    if not os.path.exists(f'./fk_result/{proj_dir}'):
-        os.makedirs(f'./fk_result/{proj_dir}')
+    if not os.path.exists(f'./fk_result/{parent_dir}/{proj_dir}'):
+        os.makedirs(f'./fk_result/{parent_dir}/{proj_dir}', exist_ok=True)
 
     data_dict = {'kp_3d_all_pe_fk': kp_3d_all.tolist()}
-    with open(f'./fk_result/{proj_dir}/kp_3d_all_pe_fk.json', 'w') as f:
+    with open(f'./fk_result/{parent_dir}/{proj_dir}/kp_3d_all_pe_fk.json', 'w') as f:
         json.dump(data_dict, f)
 
     data_dict = {'integrated_hand_rot': integrated_hand_rot}
-    with open(f'./fk_result/{proj_dir}/integrated_hand_rot.json', 'w') as f:
+    with open(f'./fk_result/{parent_dir}/{proj_dir}/integrated_hand_rot.json', 'w') as f:
         json.dump(data_dict, f)
