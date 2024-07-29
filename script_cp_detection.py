77e4ce91138ec6ccaa383ec2725c1cc14ac642a3
shiyi099 <95453996+shiyi099@users.noreply.github.com>
2024-07-30 01:39:11 +0800

    Update script_cp_detection.py

--- script_cp_detection.py
+++ script_cp_detection.py
@@ -4,18 +4,35 @@ This is the FIFTH script.
 You may need to edit: parent_dir, proj_dir, wave_file, visualize, draw_cps, draw_filtered_cps, save_position
 6. Human and instrument key points should be triangulated. (Pre-request)
 7. CONTACT POINTS DETECTION. (Run this script)
-8. INTEGRATE hand poses. (Next script)
+8. INTEGRATE hand poses. [python 'script_integrate_ik.py'](Next script)
 """
 
 import os
+import re
 from tools.Python_in_Shell import getPython3_command
 from tools.load_summary import get_folder, get_inform
 from multiprocessing import Pool
 
+def remove_chinese_and_brackets(text):
+    # remove Chinese
+    text_without_chinese = re.sub(r'[\u4e00-\u9fff]', '', text)
+    # remove special symbols
+    cleaned_text = re.sub(r'\(.*?\)|\[.*?\]|（.*?）|【.*?】', '', text_without_chinese)
+    return cleaned_text.strip()
 
 def cp_detection_process(folder_name):
+    summary, summary_jsonfile_path = get_inform(folder_name, root_path)
     proj_dir = folder_name
     wave_file = f'{root_path}/{proj_dir}/{proj_dir}.wav'
+    
+    performer = remove_chinese_and_brackets(summary['Performer'])
+    
+    if performer in ['Chongwu Wang']:
+        perform_style = 'bold'
+    else:
+        perform_style = 'normal'
+    
+    print(f"{summary['Performer']} : {perform_style}")
     print(wave_file)
     
     """
@@ -28,6 +45,7 @@ def cp_detection_process(folder_name):
                             f'--parent_dir {parent_dir} ' \
                             f'--proj_dir {proj_dir} ' \
                             f'--instrument {instrument} ' \
+                            f'--style {perform_style} ' \
                             f'--crepe_backend {crepe_backend}'
     if visualize:
         cp_detection_command += ' --visualize'
@@ -54,6 +72,7 @@ if __name__ == '__main__':
     draw_cps = False  # whether to draw contact points on virtual finger board
     draw_filtered_cps = False  # whether to draw filtered contact points on virtual finger board
     save_position = True  # whether to save filtered position
+
     crepe_backend = 'torch' # 'torch' or 'tensorflow'
     
     os.chdir('./audio/')
