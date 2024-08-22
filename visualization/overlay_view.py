cf3a497f298183ecb7b1d9fde6cbc4155928909a
shiyi099 <95453996+shiyi099@users.noreply.github.com>
2024-08-22 10:50:37 +0800

    Update overlay_view.py

--- visualization/overlay_view.py
+++ visualization/overlay_view.py
@@ -51,10 +51,12 @@ def plot_over(img, extent=None, origin="upper", dpi=100):
     img[...] = ((255 - alpha) * img.astype(np.uint16) + alpha * rgb.astype(np.uint16)) // 255
 
 
-def visualize_overlay(proj_path, overlay_cam, data, cam_num, video, start_frame = 1, parent_dir = None, saveframes = True):
+def visualize_overlay(proj_path, overlay_cam, data, cam_num, video, start_frame = 1, parent_dir = None, drop_frames_li = None, saveframes = True):
     if parent_dir is None:
         parent_dir = proj_path[:-2]
     framenum = data.shape[0]
+    print('framenum:',framenum)
+    
     
     if data.shape[1] == 155:
         from triangulation.triangulation_pipeline import HUMAN_LINKS, CELLO_LINKS as INSTRMENT_LINKS, CELLO_STRING_LINKS as STRING_LINKS, CELLO_BOW_LINKS as BOW_LINKS
@@ -70,61 +72,90 @@ def visualize_overlay(proj_path, overlay_cam, data, cam_num, video, start_frame 
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(f'./reproj_result/{parent_dir}/{proj_path}/{proj_path}_{overlay_cam}_overlay.avi', fourcc, fps=30, frameSize=[2300, 2656])
     
+    actual_frame = 0
+    actual_frame_without_dropped = 0
+    draw_skeleton = True
     for f in tqdm(range(framenum), desc=f'(Overlay -> "{proj_path}")'):
         kp_2d = data[f]
         
         #img = imageio.v2.imread(f"{img_path}_{f + start_frame}.jpg")
         #img_with_plot = img.copy()
-        img = np.asarray(video.get_data(f + start_frame - 1), dtype=np.uint8)
+        
+        if drop_frames_li is not None:
+            drop_frames_li = sorted(drop_frames_li)
+            if f == 0:
+                actual_frame_without_dropped = f + start_frame
+            else:
+                actual_frame_without_dropped += 1
+            dropped_frame = len(list(filter(lambda x:x<=actual_frame_without_dropped, drop_frames_li)))
+            actual_frame = actual_frame_without_dropped - 1 - dropped_frame
+            if actual_frame_without_dropped in drop_frames_li:
+                draw_skeleton = False
+            else:
+                draw_skeleton = True
+        else:
+            actual_frame = f + start_frame - 1
+            dropped_frame = 0  
+        img = np.asarray(video.get_data(actual_frame), dtype=np.uint8)
         img = frame_rotate(cam_num, img)
-        img_with_plot = img.copy()
+        
+        plotframetext = str(max(actual_frame + 1 + dropped_frame, actual_frame_without_dropped))
+        if draw_skeleton:
+            img_with_plot = img.copy()
+            plotdroptext = ''
+        else:
+            img_with_plot = np.zeros((img.shape),dtype = np.uint8)
+            plotdroptext = '(dropped frame)'
 
         # TODO edit the violin indices
         with plot_over(img_with_plot) as axes:
-            # human body
-            axes.scatter(kp_2d[0:133, 0],
-                         kp_2d[0:133, 1], c='#1f77b4', s=50, zorder=5)
-            # instrument body
-            axes.scatter(kp_2d[133:140-offset, 0],
-                         kp_2d[133:140-offset, 1], c='saddlebrown', s=50, zorder=13)
-            axes.scatter(kp_2d[140-offset:142-offset, 0],
-                         kp_2d[140-offset:142-offset, 1], c='cyan', s=50, zorder=13)
-            axes.scatter(kp_2d[142-offset:150-offset, 0],
-                         kp_2d[142-offset:150-offset, 1], c='w', s=50, zorder=13)
-            # axes.scatter(kp_2d[142, 0],
-            #              kp_2d[142, 1], c='r', s=50,
-            #              zorder=100)
-            if True not in np.isnan(kp_2d[150-offset]):
-                axes.scatter(kp_2d[150-offset, 0],
-                             kp_2d[150-offset, 1], c='r', s=50,
-                             zorder=100)  # zorder must be the biggest so that it would not be occluded
-                # axes.scatter(kp_2d[147:152, 0],
-                #              kp_2d[147:152, 1], c='orange', s=50,
-                #              zorder=99)
-            # else:
-            #     print(f'Frame {f} contact point not exist.')
+            if draw_skeleton:
+                # human body
+                axes.scatter(kp_2d[0:133, 0],
+                             kp_2d[0:133, 1], c='#1f77b4', s=50, zorder=5)
+                # instrument body
+                axes.scatter(kp_2d[133:140-offset, 0],
+                             kp_2d[133:140-offset, 1], c='saddlebrown', s=50, zorder=13)
+                axes.scatter(kp_2d[140-offset:142-offset, 0],
+                             kp_2d[140-offset:142-offset, 1], c='cyan', s=50, zorder=13)
+                axes.scatter(kp_2d[142-offset:150-offset, 0],
+                             kp_2d[142-offset:150-offset, 1], c='w', s=50, zorder=13)
+                # axes.scatter(kp_2d[142, 0],
+                #              kp_2d[142, 1], c='r', s=50,
+                #              zorder=100)
+                if True not in np.isnan(kp_2d[150-offset]):
+                    axes.scatter(kp_2d[150-offset, 0],
+                                 kp_2d[150-offset, 1], c='r', s=50,
+                                 zorder=100)  # zorder must be the biggest so that it would not be occluded
+                    # axes.scatter(kp_2d[147:152, 0],
+                    #              kp_2d[147:152, 1], c='orange', s=50,
+                    #              zorder=99)
+                # else:
+                #     print(f'Frame {f} contact point not exist.')
 
-            for human in HUMAN_WITHOUT_LH_THUMB_LINKS:
-                plt.plot([kp_2d[human[0]][0], kp_2d[human[1]][0]], [kp_2d[human[0]][1], kp_2d[human[1]][1]],
-                          c='b',linewidth=3, zorder=11)
-            for human in LH_THUMB_LINKS:
-                plt.plot([kp_2d[human[0]][0], kp_2d[human[1]][0]], [kp_2d[human[0]][1], kp_2d[human[1]][1]],
-                          c='b', linewidth=3, zorder=11)
-            for instrument in INSTRMENT_LINKS:
-                plt.plot([kp_2d[instrument[0]][0], kp_2d[instrument[1]][0]], [kp_2d[instrument[0]][1], kp_2d[instrument[1]][1]],
-                         c='saddlebrown', zorder=10)
-            for bow in BOW_LINKS:
-                plt.plot([kp_2d[bow[0]][0], kp_2d[bow[1]][0]], [kp_2d[bow[0]][1], kp_2d[bow[1]][1]], 
-                         c='cyan', zorder=10)
+                for human in HUMAN_WITHOUT_LH_THUMB_LINKS:
+                    plt.plot([kp_2d[human[0]][0], kp_2d[human[1]][0]], [kp_2d[human[0]][1], kp_2d[human[1]][1]],
+                              c='b',linewidth=3, zorder=11)
+                for human in LH_THUMB_LINKS:
+                    plt.plot([kp_2d[human[0]][0], kp_2d[human[1]][0]], [kp_2d[human[0]][1], kp_2d[human[1]][1]],
+                              c='b', linewidth=3, zorder=11)
+                for instrument in INSTRMENT_LINKS:
+                    plt.plot([kp_2d[instrument[0]][0], kp_2d[instrument[1]][0]], [kp_2d[instrument[0]][1], kp_2d[instrument[1]][1]],
+                             c='saddlebrown', zorder=10)
+                for bow in BOW_LINKS:
+                    plt.plot([kp_2d[bow[0]][0], kp_2d[bow[1]][0]], [kp_2d[bow[0]][1], kp_2d[bow[1]][1]], 
+                             c='cyan', zorder=10)
 
-            for string in STRING_LINKS:
-                plt.plot([kp_2d[string[0]][0], kp_2d[string[1]][0]], [kp_2d[string[0]][1], kp_2d[string[1]][1]], 
-                         c='w', zorder=10)
-        
-            plt.text(x = img_with_plot.shape[1]-150*len(str(f + start_frame - 1)), y = 150,
-                     s = str(f + start_frame - 1), c = 'b', fontsize = 125
+                for string in STRING_LINKS:
+                    plt.plot([kp_2d[string[0]][0], kp_2d[string[1]][0]], [kp_2d[string[0]][1], kp_2d[string[1]][1]], 
+                             c='w', zorder=10)
+            else:
+                plt.text(x = img_with_plot.shape[1]-150*len(plotframetext), y = 225,
+                         s = plotdroptext,fontsize = 30,c = 'b')
+            
+            plt.text(x = img_with_plot.shape[1]-150*len(plotframetext), y = 150,
+                     s = plotframetext, c = 'b', fontsize = 125
                     )
-        
         img_with_plot = img_with_plot[:, :, ::-1]
         #img_with_plot = cv2.putText(img_with_plot, str(f + start_frame - 1), (img_with_plot.shape[1]-150*len(str(f + start_frame - 1)), 150), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 255), 2)
         # vis = cv2.resize(img_with_plot, (1150, 1328))
@@ -136,7 +167,7 @@ def visualize_overlay(proj_path, overlay_cam, data, cam_num, video, start_frame 
         out.write(img_with_plot)
         plt.close()
     
-    if not saveframes:
+    if not saveframes and os.path.exists(f'./reproj_result/{parent_dir}/{proj_path}/{overlay_cam}'):
         os.removedirs(f'./reproj_result/{parent_dir}/{proj_path}/{overlay_cam}')
 
 
@@ -171,7 +202,7 @@ def overlay_process(proj_dir):
                 'cam23': 21334210
             }
     
-    cam_dict_index = 10
+    cam_dict_index = 0
     overlay_cam = str(list(CAM_DICT.values())[cam_dict_index])
     cam_num = str(list(CAM_DICT.keys())[cam_dict_index])
     
@@ -181,6 +212,7 @@ def overlay_process(proj_dir):
     video_path = os.path.join(root_path,proj_dir,f'{proj_dir}_{overlay_cam}.avi')
     video = imageio.get_reader(os.path.abspath(video_path), 'ffmpeg')
     cam_param = summary['CameraParameter']
+    drop_frames = summary['FrameDropIDLog'][overlay_cam]
 
     with open(f'../pose_estimation/ik_result/{parent_dir}/{proj_dir}/kp_3d_ik_smooth.json', 'r') as f:
         data_dict = json.load(f)
@@ -226,8 +258,7 @@ def overlay_process(proj_dir):
             kp2d = kp2d.reshape((3,))
             kp2d = kp2d / kp2d[2:3]
             repro_2d[ff, kpt, :] = kp2d[:2]
-    
-    visualize_overlay(proj_dir, overlay_cam, repro_2d, overlay_cam, video, start_frame, parent_dir = parent_dir, saveframes = False)
+    visualize_overlay(proj_dir, overlay_cam, repro_2d, overlay_cam, video, start_frame, parent_dir = parent_dir, drop_frames_li = drop_frames, saveframes = False)
     print(f'{proj_dir} -> successful')
     print('-'*60)
     return None
@@ -235,8 +266,8 @@ def overlay_process(proj_dir):
 
 if __name__ == "__main__":
     parent_dir = 'cello'
-    instrument = 'cello'
-    proj_range = range(1,86)
+    instrument = parent_dir # 'cello' / 'violin'
+    proj_range = range(1,85+1)
     proj_dirs = [parent_dir+'0'*(2-len(str(i)))+str(i) for i in proj_range]
     print(proj_dirs)
     
